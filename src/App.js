a09f4ae59c643449f8f5e130973d5f3b67966d13
halsheik <halsheik@umich.edu>
2021-10-18 23:08:04 -0400

    added routing to the frontend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import UserRegistrationForm from "./components/Users/userRegistrationForm"
+import { BrowserRouter, Route } from 'react-router-dom'
+import { Switch } from 'react-router';
 
 function App() {
   return (
     <div className="App">
-      <UserRegistrationForm/>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/register" component={UserRegistrationForm} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
