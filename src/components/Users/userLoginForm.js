6fcdd07907447e61e3de405841f5b7823b730348
bhestand <77692117+bhestand@users.noreply.github.com>
2021-10-19 18:52:05 -0400

    added frontend component for logging in, works

---
+++ src/components/Users/userLoginForm.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import axios  from 'axios';
+
+class UserLoginForm extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            username: "",
+            password: "",
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        let name = e.target.name;
+        if(name === "username") {
+            this.setState({ username: e.target.value });
+        } else if(name === "password") {
+            this.setState({ password: e.target.value });
+        }
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post("http://localhost:8080/loginUser", this.state)
+        .catch(err => console.log(err.data))
+    }
+
+    render() {
+        const { username, password} = this.state;
+
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        <p>Username</p>
+                        <input type="text" name="username" value={username} onChange={this.handleChange} />
+                    </label>
+                    <label>
+                        <p>Password</p>
+                        <input type="text" name="password" value={password} onChange={this.handleChange} />
+                    </label>
+                    <button>Submit</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default UserLoginForm;
